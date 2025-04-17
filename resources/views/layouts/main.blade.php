6c54064b2b3e628c1d712330d2fd831d831b4cd5
Muhammad-Sufyan-901 <muhammadsufyann09@gmail.com>
2025-04-17 20:28:17 +0800

    fix : fixing modal button color bug

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,11 @@
             text: "{{ Session::get('error') ?? ($error ?? '') }}",
             icon: 'error',
             confirmButtonText: 'OK',
-            confirmButtonColor: 'rgb(37, 99, 235)',
+            customClass: {
+                confirmButton: 'custom-decline-button'
+            },
+            buttonsStyling: true,
+            confirmButtonColor: '#dc3545'
         })
     </script>
 @endif
@@ -46,6 +50,16 @@
             document.documentElement.classList.remove('dark')
         }
     </script>
+
+    <style>
+        .custom-confirm-button {
+            background-color: #1d4ed8;
+        }
+
+        .custom-decline-button {
+            background-color: #dc3545;
+        }
+    </style>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
@@ -92,14 +106,17 @@
     @if (Session::has('success'))
         {{-- Untuk !empty dapat dihilangkan nanti saat redirect page sudah diimplementasikan (untuk menghindari bug alert) --}}
         {{-- Contoh @if (Session::has('success')) --}}
-
         <script>
             Swal.fire({
                 title: 'Success!',
                 text: "{{ Session::get('success') ?? ($success ?? '') }}",
                 icon: 'success',
                 confirmButtonText: 'OK',
-                confirmButtonColor: 'rgb(37, 99, 235)',
+                customClass: {
+                    confirmButton: 'custom-confirm-button'
+                },
+                buttonsStyling: true,
+                confirmButtonColor: '#1d4ed8' // Change this to your desired color
             })
         </script>
     @endif
