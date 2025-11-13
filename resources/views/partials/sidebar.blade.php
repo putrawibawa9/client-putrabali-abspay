6016618daf965116303a8e616e80446bead66491
putrawibawa9 <putrawibawa7@gmail.com>
2025-11-13 23:40:30 +0800

    coba lagi push

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -163,10 +163,10 @@
                                     class="text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100 transition duration-75 pl-11 dark:text-gray-200 dark:hover:bg-gray-700 {{ $activeRoute == 'payments' ? 'bg-gray-100 dark:bg-gray-700' : '' }} ">Pembayaran
                                     </a>
                             </li>
-                            <li>
+                            {{-- <li>
                                 <a href="/course-prices"
                                     class="text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100 transition duration-75 pl-11 dark:text-gray-200 dark:hover:bg-gray-700 {{ $activeRoute == 'course-prices' ? 'bg-gray-100 dark:bg-gray-700' : '' }} ">Pembayaran Bulanan</a>
-                            </li>
+                            </li> --}}
                            
                         </ul>
                     </li>
