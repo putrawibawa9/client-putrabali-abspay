8c9939df4beaab499eab7ce5e235bdb30b44bf3a
putrawibawa9 <putrawibawa7@gmail.com>
2025-12-10 14:40:43 +0800

    done

--- resources/views/pages/payment/show.blade.php
+++ resources/views/pages/payment/show.blade.php
@@ -174,7 +174,7 @@
                   <div class="flex items-start gap-2">
                     <div class="text-sm font-medium text-gray-900 dark:text-gray-300">
                       <div class="flex flex-col">
-                    <span>Bulan: {{ now()->translatedFormat('F Y') }}</span>
+                    <span> {{ now()->translatedFormat('F Y') }}</span>
 
                    Rp. {{ number_format($course['monthly_price'], 0, ',', '.') }}
 
